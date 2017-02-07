5d0b81e10d58c67161a1f8d451aba383b647578b
karthikeyan <karthikeyan@KARTHIK>
2017-02-08 00:12:26 +0530

    New Changes

---
+++ MessageSevenProject/Helper/Converters/EnumDescriptionValueConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MessageSevenProject.Helper.Converters
+{
+    public class EnumDescriptionValueConverter: IValueConverter
+    {
+        private string GetEnumDescription(Enum enumObj)
+        {
+            FieldInfo fieldInfo = enumObj.GetType().GetField(enumObj.ToString());
+
+            object[] attribArray = fieldInfo.GetCustomAttributes(false);
+
+            if (attribArray.Length == 0)
+            {
+                return enumObj.ToString();
+            }
+            else
+            {
+                DisplayNameAttribute attrib = attribArray[0] as DisplayNameAttribute;
+                return attrib.DisplayName;
+
+            }
+        }
+
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Enum myEnum = (Enum)value;
+            string description = GetEnumDescription(myEnum);
+            return description;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Empty;
+        }
+    }
+}
